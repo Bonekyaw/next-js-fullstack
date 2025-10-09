b4309759ec591496b8aefb0509f6929702758f55
bonekyaw <phonenai2014@gmail.com>
2025-10-09 20:50:14 +0630

    Component

---
+++ react-revision/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  const count = 0;
+
+  return (
+    <>
+      <h1>Counter app</h1>
+      <p className="logo">Count : {count}</p>
+    </>
+  );
+}
+
+export default App;
+
+// JSX
