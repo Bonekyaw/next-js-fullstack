2326ff51fa7e095c5d58c95775b27ca89a227161
bonekyaw <phonenai2014@gmail.com>
2025-10-10 14:08:11 +0630

    props & state

--- react-revision/src/App.tsx
+++ react-revision/src/App.tsx
@@ -1,16 +1,52 @@
+import { useState } from "react";
 import "./App.css";
 
+interface TitleProps {
+  title: string;
+}
+
+interface HeaderProps extends TitleProps {
+  noti: number;
+}
+
+const Layout = ({ children }: Readonly<React.PropsWithChildren>) => {
+  return <div className="layout">{children}</div>;
+};
+
+const Header = ({ title, noti }: HeaderProps) => {
+  return (
+    <Layout>
+      <h3>
+        {title} - {noti}
+      </h3>
+    </Layout>
+  );
+};
+
+const Footer = ({ title }: TitleProps) => {
+  return (
+    <Layout>
+      <h4>{title}</h4>
+    </Layout>
+  );
+};
+
 function App() {
-  const count = 0;
+  const [count, setCount] = useState(0);
+  const name = "Furniture Shop";
 
   return (
     <>
+      <Header title={name} noti={39} />
       <h1>Counter app</h1>
       <p className="logo">Count : {count}</p>
+      <button onClick={() => setCount(count + 1)}>+ Increase</button>
+      <Layout>App layout</Layout>
+      <Footer title="Counter Footer" />
     </>
   );
 }
 
 export default App;
 
-// JSX
+// props
