ab79a2c271d6bfa6956b25811a3efd7389becf3b
bonekyaw <phonenai2014@gmail.com>
2025-10-22 15:01:08 +0630

    SSG & ISR

--- next-shop/app/(root)/blog/authors/[authorId]/page.tsx
+++ next-shop/app/(root)/blog/authors/[authorId]/page.tsx
@@ -1,10 +1,30 @@
+// ISR - Incremental Static Regeneration
+// Time-based Revalidation
+// On-demand Revalidation
+export const dynamicParams = false; // if true, it can generate pages on the fly for new params
+
+export const revalidate = 60; // revalidate this page every 1 mins, but is should be more than 1 hours
+
+export async function generateStaticParams() {
+  // const authors = await fetch("https://api.example.com/authors").then((res) => res.json());
+  // return authors.map((author: { id: string }) => ({
+  //   authorId: author.id,
+  // }));
+  // [{ authorId: '1' }, { authorId: '2' }, ...]
+  return [{ authorId: "1" }, { authorId: "2" }, { authorId: "3" }];
+}
+
 async function AuthorDetail({
   params,
 }: {
   params: Promise<{ authorId: string }>;
 }) {
   const { authorId } = await params;
-  return <div>AuthorDetail - {authorId}</div>;
+  return (
+    <div>
+      AuthorDetail - {authorId} - {new Date().toLocaleTimeString()}
+    </div>
+  );
 }
 
 export default AuthorDetail;
