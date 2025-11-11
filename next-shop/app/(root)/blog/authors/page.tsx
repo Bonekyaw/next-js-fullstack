851360bf9e71977aa2ca820babf9801cfd77c318
bonekyaw <phonenai2014@gmail.com>
2025-11-11 20:40:53 +0630

    prisma

--- next-shop/app/(root)/blog/authors/page.tsx
+++ next-shop/app/(root)/blog/authors/page.tsx
@@ -1,5 +1,16 @@
-function Authors() {
-  return <div>Authors</div>;
+import prisma from "@/lib/prisma";
+
+async function Authors() {
+  const users = await prisma.user.findMany();
+
+  return (
+    <>
+      <div>Authors</div>
+      {users.map((user) => (
+        <h1 key={user.id}>{user.name}</h1>
+      ))}
+    </>
+  );
 }
 
 export default Authors;
