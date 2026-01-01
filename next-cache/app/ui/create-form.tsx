beab11f24feb24ea4c8c427fb1a455dd164789a8
bonekyaw <phonenai2014@gmail.com>
2026-01-01 20:12:07 +0630

    server-side validation

--- next-cache/app/ui/create-form.tsx
+++ next-cache/app/ui/create-form.tsx
@@ -1,16 +1,28 @@
 "use client";
 
+import { useActionState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { createPostAction } from "@/lib/actions/post";
+import { createPostAction, State } from "@/lib/actions/post";
 
 export default function CreatePostForm() {
+  // Testing Purpose
+  const authorId = 1;
+  const createPostActionWithId = createPostAction.bind(null, authorId);
+
+  const initialState: State = { errors: {}, message: null };
+  const [state, formAction, pending] = useActionState(
+    createPostActionWithId,
+    initialState
+  );
+
   return (
     <div className="w-full max-w-md">
-      <form action={createPostAction}>
+      <form action={formAction}>
         <FieldGroup>
           <FieldSet>
             <FieldGroup>
@@ -25,6 +37,14 @@ export default function CreatePostForm() {
                   required
                 />
               </Field>
+              <div>
+                {state.errors?.title &&
+                  state.errors?.title.map((error: string) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+              </div>
 
               <Field>
                 <FieldLabel htmlFor="checkout-7j9-optional-comments">
@@ -37,6 +57,15 @@ export default function CreatePostForm() {
                   className="resize-none"
                 />
               </Field>
+              <div>
+                {state.errors?.content &&
+                  state.errors?.content.map((error: string) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+              </div>
+
               <Field orientation="horizontal">
                 <Checkbox
                   id="checkout-7j9-same-as-shipping-wgm"
@@ -50,10 +79,20 @@ export default function CreatePostForm() {
                   This post will be published right now.
                 </FieldLabel>
               </Field>
+              <div>
+                {state.errors?.published &&
+                  state.errors?.published.map((error: string) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+              </div>
             </FieldGroup>
           </FieldSet>
           <Field orientation="horizontal">
-            <Button type="submit">Submit</Button>
+            <Button type="submit" disabled={pending}>
+              {pending ? "Submitting..." : "Submit"}
+            </Button>
             <Button variant="outline" type="reset">
               Reset
             </Button>
--- next-cache/lib/actions/post.ts
+++ next-cache/lib/actions/post.ts
@@ -1,9 +1,67 @@
 "use server";
 
-export async function createPostAction(formData: FormData) {
-  const title = formData.get("title");
-  const content = formData.get("content");
-  const published = formData.get("published");
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
-  console.log(title, content, published);
+import prisma from "@/lib/prisma";
+
+export type State = {
+  errors?: {
+    title?: string[];
+    content?: string[];
+    published?: string[];
+  };
+  message?: string | null;
+};
+
+const FormSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(5, "Title is invalid!")
+    .max(100, "Title is too long!"),
+  content: z.string().trim().min(10, "Content is invalid!"),
+  published: z
+    .string()
+    .nullable()
+    .transform((val) => val === "on"),
+});
+
+export async function createPostAction(
+  authorId: number,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = FormSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+    published: formData.get("published"),
+  });
+
+  if (!validatedFields.success) {
+    const flattened = z.flattenError(validatedFields.error);
+    return {
+      errors: flattened.fieldErrors,
+      message: "Failed to create a new post",
+    };
+  }
+
+  const { title, content, published } = validatedFields.data;
+
+  try {
+    await prisma.post.create({
+      data: {
+        title,
+        content,
+        published,
+        authorId,
+      },
+    });
+  } catch {
+    return {
+      message: "Database Error: Failed to create a new post",
+    };
+  }
+
+  redirect("/posts");
 }
