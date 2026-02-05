76b4efa81e589c1553323db6a797fdd82d15444e
bonekyaw <phonenai2014@gmail.com>
2026-02-05 17:02:39 +0630

    better-auth configuration

--- fashion-shop/lib/auth.ts
+++ fashion-shop/lib/auth.ts
@@ -1,12 +1,40 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  // trustedOrigins: ["http://localhost:5173"],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      prompt: "select_account", // Always prompt to select account
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        input: false,
+      },
+    },
+  },
+  // session: {
+  //       cookieCache: {
+  //           enabled: true,
+  //           maxAge: 5 * 60, // 5 minutes
+  //       }
+  //   },
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
+
+export type Session = typeof auth.$Infer.Session;
+export type User = typeof auth.$Infer.Session.user;
